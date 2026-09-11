9263e808fc9e4c1efb85548fbdd7cd98e1773069
Eduardo <eduardoogrand@gmail.com>
2026-09-11 10:27:34 -0300

    Metodos do Projeto para Main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
-public class Main{
-    public static void main (String[] args){
-        System.out.println("=============================");
-        System.out.println("    Sistema de Projetos    ");
-        System.out.println("=============================");
-        System.out.println("");
-        System.out.println("Projeto: Portfólio Acadêmico");
-        System.out.println("Desenvolvido em Java");
-        System.out.println("Versão: 1.0");
+import model.Projeto;
+
+public class Main {
+    public static void main(String[] args) {
+        Projeto projeto = new Projeto(
+            1,
+            "Sistema Acadêmico",
+            "Sistema para gerenciamento acadêmico",
+            "Software",
+            "Em desenvolvimento"
+        );
+        
+        Projeto projeto2 = new Projeto(
+            2,
+            "Site Institucional",
+            "Website de uma instituição",
+            "Web",
+            "Concluído"
+
+        );
+        
+        projeto.exibirDados();
+
+        System.out.println();
+
+        projeto2.exibirDados();
     }
 }
