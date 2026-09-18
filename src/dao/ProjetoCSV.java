a899d26f36be36199e63a6809ac3ac81caa968e8
Eduardo <eduardoogrand@gmail.com>
2026-09-18 10:19:27 -0300

    Create ProjetoCSV.java

---
+++ src/dao/ProjetoCSV.java
@@ -0,0 +1,41 @@
+package dao;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import model.Projeto;
+
+public class ProjetoCSV {
+
+    private Path caminho;
+
+    public ProjetoCSV() {
+
+        caminho =
+            Path.of("src/dao/projetos.csv");
+
+    }
+
+    public void salvar(List<Projeto> projetos)
+        throws Exception {
+
+        List<String> linhas =  new ArrayList<>();
+
+        linhas.add("id;nome;descricao;categoria;status");
+
+        for (Projeto projeto : projetos) {
+
+            String linha =
+                projeto.getId() + ";" +
+                projeto.getNome() + ";" +
+                projeto.getDescricao() + ";" +
+                projeto.getCategoria() + ";" +
+                projeto.getStatus();
+
+            linhas.add(linha);
+        }
+
+        Files.write(caminho, linhas);
+    }
+}
